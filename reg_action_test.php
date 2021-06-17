182bf6f77d1c53bd24b85ec788903ed89624c842
Madhawa Monarawila <madawammonarawila@gmail.com>
2021-06-17 18:05:30 +0530

    altered registration to stop duplicating email address in data base: users can't register with a already used email address

--- reg_action_test.php
+++ reg_action_test.php
@@ -1,6 +1,5 @@
 <?php
-//To start session
-		// if(!isset($_SESSION)){
+
 session_start();
 
 // require("connection.php");
@@ -17,13 +16,24 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "INSERT INTO car_owners (f_name,l_name,nic,city,email,password) VALUES('$_POST[first_name]','$_POST[last_name]','$_POST[nic]','$_POST[city]','$_POST[email]','$_POST[password]')";
+//--- see if the user is exists
+$sql = "SELECT email from car_owners WHERE email = '$_POST[email]' " ; 
+$result = $conn->query($sql);
 
-if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully! Thank You for registering with Us !";	
-		
+if ($result -> num_rows > 0) {
+    echo "Couldn't create account. You're already registered with this email.";
+    
 } else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+    //---- data insert part
+    $sql_2 = "INSERT INTO car_owners (f_name,l_name,nic,city,email,password) VALUES('$_POST[first_name]','$_POST[last_name]','$_POST[nic]','$_POST[city]','$_POST[email]','$_POST[password]')";
+    if ($conn->query($sql_2) === TRUE) {
+        echo "New record created successfully! Thank You for registering with Us !";	
+            
+    } else {
+        echo "Error: " . $sql_2 . "<br>" . $conn->error;
+    }
+    //---- end of data insert
+
 }
 
 $conn->close();
