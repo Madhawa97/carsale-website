5a3bd14d4d1c514892dc57971b8e3a7d31e37dfd
Madhawa Monarawila <madawammonarawila@gmail.com>
2021-06-17 18:22:51 +0530

    added iframmed php file on search.php as auto_car_list.php; changes in search.php doesn't matter

--- search.php
+++ search.php
@@ -3,18 +3,18 @@
 
 // require("connection.php");
 
-$servername = "localhost";
+$server = "localhost";
 $username = "root";
 $pass = "root";
 $dbname = "car_sale";
 
 // Create connection
-$conn = new mysqli($servername, $username, $pass, $dbname);
+$conn = new mysqli($server, $username, $pass, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-        
+
 $sql = "SELECT DISTINCT brand FROM car_info ";
 $result_set = $conn->query($sql);
 
@@ -22,9 +22,8 @@ $brand_list = "";
 	while ( $result = mysqli_fetch_assoc($result_set) ) {
 		$brand_list .= "<option value=\"{$result['brand']}\">{$result['brand']}</option>";
 	}
-    //------------
+    //----------
     // echo $brand_list;
-
     //----------
 ?>
 
@@ -58,6 +57,9 @@ $brand_list = "";
         <input type="submit" value="Search">
     </form>
 
+    <br><br>
+    <iframe src="auto_car_list.php" title="ALL LISTED CARS" width=1500px height=1000px></iframe>
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.0/jquery.min.js"></script>	
     <script>
         $(document).ready(function(){
@@ -69,8 +71,6 @@ $brand_list = "";
                 $.get(getURL, function(data, status){
                     console.log(data);
                     $('#model').html(data);
-                    // console.log(brand);
-                    // console.log(data);
                 });
             });
         });
